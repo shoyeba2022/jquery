40ca59bfcbd3e749a580d833cfaed4ecf64e16b0
shoyeba2022 <shoyebarazzak1998@gmail.com>
2023-11-22 20:14:48 +0600

    firstadd

---
+++ test.js
@@ -0,0 +1,29 @@
+
+$(function () {
+    $('#typewriter').typewrite({
+        text: ["jQuery typeWriter.js", "A jQuery Plugin",
+            "Typing Animation", "jQueryScript.Net"],
+        speed: 100
+    }
+    );
+});
+// 2nd js
+google_ad_client = "ca-pub-2783044520727903";
+/* jQuery_demo */
+google_ad_slot = "2780937993";
+google_ad_width = 728;
+google_ad_height = 90;
+//-->
+// 3rd js
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-36251023-1']);
+_gaq.push(['_setDomainName', 'jqueryscript.net']);
+_gaq.push(['_trackPageview']);
+
+(function () {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
